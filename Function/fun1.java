f8e543d51d4986428ddfa4085840a36de98152c5
sharma-pranita <sharmapranita707008@gmail.com>
2025-04-05 13:45:19 +0530

    function

---
+++ Function/fun1.java
@@ -0,0 +1,19 @@
+package Function;
+
+public class fun1 {
+
+  public static void main(String[] args) {
+    // Scanner in = new Scanner(System.in);
+    // System.out.println("Enter the value of a: ");
+    // int a = in.nextInt();
+    // System.out.println("Enter the value of b: ");
+    // int b = in.nextInt();
+    // calc(a, b);
+    calc(1,2);
+  }
+
+  static void calc(int a, int b) {
+    int sum = a + b;
+    System.out.println("The sum of a and b is : " + sum);
+  }
+}
