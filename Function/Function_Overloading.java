60b4ac0270c2eae62cdf9c14cadb03766729c36a
sharma-pranita <sharmapranita707008@gmail.com>
2025-04-06 21:57:59 +0530

    Function overloading

---
+++ Function/Function_Overloading.java
@@ -0,0 +1,14 @@
+package Function;
+
+public class Function_Overloading {
+    public static void main(String[] args) {
+        // fun();
+    }
+
+    static void fun(int a){
+
+    }
+    static void fun(int b,int c){
+
+    }
+}
