77ff8bebc7e2679592f3d7123a80dcf021f808c2
sharma-pranita <sharmapranita707008@gmail.com>
2025-04-10 22:39:43 +0530

    Swapping Array

---
+++ Array_1/Swap_Array.java
@@ -0,0 +1,17 @@
+package Array_1;
+import java.util.Arrays;
+public class Swap_Array {
+    public static void main(String[] args) {
+        int[] arr = {22,13,15,18};
+        System.out.println(Arrays.toString(arr));
+        swap(arr,1,2);
+        System.out.println("Swapped array in main function "+Arrays.toString(arr));
+    }
+
+    static void swap(int[] arr,int index1 , int index2){
+        int temp = arr[index1];
+        arr[index1] = arr[index2];
+        arr[index2] = temp;
+         System.out.println("Swapped array in Swap function "+Arrays.toString(arr));
+    }
+}
