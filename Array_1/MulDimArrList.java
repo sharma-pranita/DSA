2f426d0e4f87a3370033bff68ce2a7ae7f08e992
sharma-pranita <sharmapranita707008@gmail.com>
2025-04-10 22:39:26 +0530

    MultiDimensional ArrayLIst

---
+++ Array_1/MulDimArrList.java
@@ -0,0 +1,30 @@
+package Array_1;
+import java.util.*;
+public class MulDimArrList {
+    
+public static void main(String[] args) {
+    ArrayList<ArrayList <Integer>> List = new ArrayList<>(10);
+Scanner in = new Scanner(System.in);
+
+
+   //Initialization
+
+   for(int i=0;i<3;i++){
+    List.add(new ArrayList<>());
+   }
+
+   System.out.println(List);
+//    Input
+// System.out.print("Enter 9 array elements ");
+// for(int i =0;i<3;i++){
+//     for(int j=0;j<3;j++){
+//         List.get(i).add(in.nextInt());
+//     }
+// }
+
+//Print
+// System.out.println(List);
+
+}
+    
+}
