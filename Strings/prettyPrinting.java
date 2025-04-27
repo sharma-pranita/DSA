cc8e0612c818a5c08213d80965b207018bd442c5
sharma-pranita <sharmapranita707008@gmail.com>
2025-04-27 18:17:39 +0530

    pretty printing

---
+++ Strings/prettyPrinting.java
@@ -0,0 +1,28 @@
+package Strings;
+
+public class prettyPrinting {
+
+  public static void main(String[] args) {
+    // float a = 78.1693f;
+    // System.out.printf("The formatted no is %.2f",a);
+    // System.out.printf("The formatted no is %.2f"+ a);
+
+    // int a1 = 10;
+    // float b = 12.34567f;
+    // String name = "Pranita";
+    // char ch = 'P';
+    // boolean status = true;
+
+    // System.out.printf("Name: %s%n", name); // String
+    // System.out.printf("Score: %.2f%n", b); // Float with 2 decimals
+    // System.out.printf("Character: %c%n", ch); // Character
+    // System.out.printf("Status: %b%n", status); // Boolean
+    // System.out.printf("Integer: %d%n", a); // Integer
+    // System.out.printf("Percentage: 100%%%n"); // Prints: 100%
+
+
+    // System.out.printf("The value of pi is %.3f",Math.PI);
+
+    System.out.printf("Hey my name is %s and i am a %s student","Pranita","Btech");
+  }
+}
